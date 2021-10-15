34713759eb07deadf12b79e0331b5e239bce069f
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-15 08:37:52 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+   for i in range(20): # adjust integer value for need
+       # you can change right side number for scroll convenience or destination 
+       driver.execute_script("window.scrollBy(0, 250)")
+       # you can change time integer to float or remove
+       time.sleep(1)
